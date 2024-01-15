923f120323c81fb9aa0fbdeb234c0d832d2285de
Xavier Valcarce <xvalcarce@protonmail.ch>
2024-01-15 21:53:09 +0100

    fix: help depreciated for vimdoc in nvim-treesitter

--- after/plugin/nvim-treesitter.lua
+++ after/plugin/nvim-treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "help", "c", "lua", "julia", "python", "javascript", "bash"},
+  ensure_installed = { "vimdoc", "c", "lua", "julia", "python", "javascript", "bash"},
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
